4b8d1c5a3c18aa4827e31cf4ac2233b30572f31d
kakihanatakeyukirou <kakihanatakeyukirou@localhost>
2023-04-26 14:44:49 +0900

    変更　課題３　新規　課題４
    課題３　インデント揃える
    課題４　新規追加　課題準備

--- HelloWorld/src/sample/Sample.java
+++ HelloWorld/src/sample/Sample.java
@@ -2,9 +2,9 @@ package sample;
 
 public class Sample {
 
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
-		System.out.println("Hello,World");
-	}
+			System.out.println("Hello,World");
+		}
 
 }
