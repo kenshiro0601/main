991119404ca2106869e3eabbaa0bb9bbc66f20cc
kakihanatakeyukirou <kakihanatakeyukirou@localhost>
2023-05-01 23:36:56 +0900

    (再)5/1 Java基礎 1.3 Java導入 / 1.4 Javaプログラミング
    インデントの修正

--- HelloWorld/src/sample/Sample.java
+++ HelloWorld/src/sample/Sample.java
@@ -2,13 +2,13 @@ package sample;
 
 public class Sample {
 
-		public static void main(String[] args) {
-// 11110
-// 「c」という箱
-			int c;
-// （cの箱に11110を入れる）
-			c = 11110;
-// 箱の中身を表示
-			System.out.println(c);
-		}
+	public static void main(String[] args) {
+		// 11110
+		// 「c」という箱
+		int c;
+		// （cの箱に11110を入れる）
+		c = 11110;
+		// 箱の中身を表示
+		System.out.println(c);
+	}
 }
